82edb6c09f8db7cad5f2ea34abc4c615341da39f
Antonio Rosales Martínez <antoniorosalesmartinez@MacBook-Pro-de-Antonio.local>
2022-11-28 20:25:24 +0100

    feat: portfolio

--- Javascript/21-vite/portfolio/main.js
+++ Javascript/21-vite/portfolio/main.js
@@ -5,6 +5,7 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Footer } from "./components/Footer/Footer";
 import { Home } from "./pages/Home/Home";
 import { Projects } from "./pages/Projects/Projects";
+import { Divider } from "./components/Divider/Divider";
 
 const header = document.querySelector("header");
 header.innerHTML = Navbar();
@@ -17,3 +18,5 @@ linkPage("#projectslink", Projects);
 Home();
 
 changeTheme();
+
+footer.insertAdjacentHTML("beforebegin", Divider());
--- Javascript/21-vite/portfolio/pages/Projects/Projects.js
+++ Javascript/21-vite/portfolio/pages/Projects/Projects.js
@@ -1,29 +1,22 @@
 import "./Projects.css";
 import { cleanPage } from "../../utils/cleanPage";
 import { projects } from "../../data/projects";
+import { ProjectCard } from "../../components/ProjectCard/ProjectCard";
+import { Divider } from "../../components/Divider/Divider";
 
 export const Projects = () => {
   const main = document.querySelector("main");
   cleanPage(main);
   main.innerHTML = `
     <section class="projects">
-    <p>Featured Projects</p>
+    <h2>Featured Projects</h2>
+    ${Divider()}
     <div class="projects-container"></div>
     </section>`;
   const container = document.querySelector(".projects-container");
   for (const project of projects) {
     const figure = document.createElement("figure");
-    figure.innerHTML = `
-    <img src=${project.image} alt=${project.title}/>
-    <div>
-    <h2>${project.title}</h2>
-    <div>
-    Icons
-    </div>
-    <p>${project.description}</p>
-    <p>${project.tech.join(" - ")}</p>
-    </div>
-    `;
+    figure.innerHTML = ProjectCard(project);
     container.appendChild(figure);
   }
 };
